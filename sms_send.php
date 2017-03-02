e0723dd399c91e62efbf2a52cf9ae2bdbb08d0bd
Prabhunathan <prabhunathan.g@gmail.com>
2017-03-02 01:27:41 -0600

    changed phone number

--- sms_send.php
+++ sms_send.php
@@ -8,17 +8,17 @@ require __DIR__ . '/twilio-php-master/Twilio/autoload.php';
 use Twilio\Rest\Client;
 
 // Your Account SID and Auth Token from twilio.com/console
-$sid = 'AC133beb918db59e93c2e840d53b223b23';
-$token = '339b4a2c38b641953a167f295c5887b9';
+$sid = 'xxxxx';
+$token = 'xxxxx';
 $client = new Client($sid, $token);
 
 // Use the client to do fun stuff like send text messages!
 $client->messages->create(
     // the number you'd like to send the message to
-    '+17739499557',
+    '+xxxxxxxxxx',
     array(
         // A Twilio phone number you purchased at twilio.com/console
-        'from' => '+18722216725',
+        'from' => '+xxxxxxxxxx',
         // the body of the text message you'd like to send
         'body' => "CDC is hosting awareness event on preventing plague. Please attend on Nov 20, 2016 at Mark Hall, St. Louis, MO"
     )
